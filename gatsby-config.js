a09aa4a6707d63400bcec0d2056527a0f1db5602
Jina <wonjieunkk@gmail.com>
2021-11-02 20:14:09 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    siteUrl: "https://dewpeng.com",
+    title: "Jina❤️",
   },
   plugins: [
     "gatsby-plugin-image",

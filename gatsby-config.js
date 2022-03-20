57d1f33e99c7942ad7de77b5e3144aba66b732bd
Jina <wonjieunkk@gmail.com>
2022-03-20 14:25:56 +0900

    📖 DOC: reference

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://meblog.gatsbyjs.io",
-    title: "Jina❤️",
+    siteUrl: 'https://meblog.gatsbyjs.io',
+    title: 'Jina❤️',
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem', // @see https://www.gatsbyjs.com/docs/how-to/sourcing-data/
       options: {
-        name: `src`,
+        name: 'src',
         path: `${__dirname}/src/`,
       },
     },
